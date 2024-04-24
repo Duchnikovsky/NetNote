53adea99f069268cef2b9e5d9f647073f1d2e676
Duchnikovsky <filipduchnik@outlook.com>
2024-04-24 14:33:31 +0200

    fix(index): Modify main and scripts fields in package.json

--- back-end/index.ts
+++ back-end/index.ts
@@ -2,6 +2,8 @@ const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
+const dotenv = require("dotenv");
+dotenv.config();
 
 import { PrismaClient } from "@prisma/client";
 export const prisma = new PrismaClient();
